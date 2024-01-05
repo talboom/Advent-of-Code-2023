6d5b9b37cbea0b4be8f597a2ce128dbb6b59c0f0
Talboom <t_talboom@hotmail.com>
2024-01-05 21:02:14 +0100

    Add day 17

--- day17.py
+++ day17.py
@@ -1,30 +1,104 @@
 from utils import read_input
+import time
+from collections import defaultdict
 
-input = read_input("day17_test.txt")
-grid = [c for s in input for c in s]
+input = read_input("day17.txt")
+nodes = [list(int(x) for x in line) for line in input]
 
-h,w = len(grid), len(grid[0])
-grid_loss = [[0]*w for _ in range(h)]
+directions = [(-1,0),(0,1),(1,0),(0,-1)]
 
-pos = (0,0)
-direction = (0,1)
+# part 1
 
-for i, line in enumerate(grid):
-    for j,p in enumerate(line):
-        for x in range(i):
-            for y in range(j):
-                grid_loss[x][y] =
+# cost, pos, direction, num_steps
+paths = defaultdict(list)
+paths[0].append([(0,0), 1, 0])
+paths[0].append([(0,0), 2, 0])
 
-2 4
-3 2
-    
-0 4
-3 5
+target = (len(nodes)-1, len(nodes[0])-1)
+h, w = target
+visted = set()
 
-2 4 1
-3 2 1
-3 2 5
-    
-0 4 5
-3 5 6
-6 7 11
+
+start_time = time.time()
+
+c = 0 
+
+while paths:
+    while not paths[c]:
+        c += 1
+
+    path = paths[c].pop(0)        
+
+    if path[0] == target:
+        print(c)
+        break
+
+    if (path[0], path[1], path[2]) in visted:
+        continue
+    visted.add((path[0], path[1], path[2]))
+
+    l = (path[0][0] + directions[(path[1] - 1) % 4][0], path[0][1] + directions[(path[1] - 1) % 4][1])
+    r = (path[0][0] + directions[(path[1] + 1) % 4][0], path[0][1] + directions[(path[1] + 1) % 4][1])
+    f = (path[0][0] + directions[path[1]][0], path[0][1] + directions[path[1]][1])
+
+
+    if 0 <= l[0] <= h and 0 <= l[1] <= w:
+        paths[c+nodes[l[0]][l[1]]].append([ l, (path[1]-1)%4, 1]) 
+    if 0 <= r[0] <= h and 0 <= r[1] <= w:
+        paths[c+nodes[r[0]][r[1]]].append([r, (path[1]+1)%4, 1]) 
+    if 0 <= f[0] <= h and 0 <= f[1] <= w and path[2] < 3:
+        paths[c+nodes[f[0]][f[1]]].append([f, path[1], path[2]+1]) 
+
+end_time = time.time()
+execution_time = end_time - start_time
+
+print(f"Execution time: {execution_time} seconds")
+
+
+# part 2
+
+# cost, pos, direction, num_steps
+paths = defaultdict(list)
+paths[0].append([(0,0), 1, 0])
+paths[0].append([(0,0), 2, 0])
+
+target = (len(nodes)-1, len(nodes[0])-1)
+h, w = target
+visted = set()
+
+
+start_time = time.time()
+
+c = 0 
+
+while paths:
+    while not paths[c]:
+        c += 1
+
+    path = paths[c].pop(0)        
+
+    if path[0] == target and path[2] >= 4:
+        print(c)
+        break
+
+    if (path[0], path[1], path[2]) in visted:
+        continue
+    visted.add((path[0], path[1], path[2]))
+
+    l = (path[0][0] + directions[(path[1] - 1) % 4][0], path[0][1] + directions[(path[1] - 1) % 4][1])
+    r = (path[0][0] + directions[(path[1] + 1) % 4][0], path[0][1] + directions[(path[1] + 1) % 4][1])
+    f = (path[0][0] + directions[path[1]][0], path[0][1] + directions[path[1]][1])
+
+
+    if 0 <= l[0] <= h and 0 <= l[1] <= w and path[2] >= 4:
+        paths[c+nodes[l[0]][l[1]]].append([ l, (path[1]-1)%4, 1]) 
+    if 0 <= r[0] <= h and 0 <= r[1] <= w and path[2] >= 4:
+        paths[c+nodes[r[0]][r[1]]].append([r, (path[1]+1)%4, 1]) 
+    if 0 <= f[0] <= h and 0 <= f[1] <= w and path[2] < 10:
+        paths[c+nodes[f[0]][f[1]]].append([f, path[1], path[2]+1]) 
+
+end_time = time.time()
+execution_time = end_time - start_time
+
+print(f"Execution time: {execution_time} seconds")
+
