dbcf1c527b03ae53442651f18cd8ff5f6eb89e41
Talboom <t_talboom@hotmail.com>
2024-12-01 10:16:18 +0100

    Make day 1 shorter

--- 2024/day1.py
+++ 2024/day1.py
@@ -4,20 +4,12 @@ from utils import read_input
 
 data = read_input("day1.txt")
 
-sum_res = 0
-list_1 = []
-list_2 = []
-for d in data:
-    locations = [int(x) for x in re.findall(r'\d+', d)]
-    list_1.append(locations[0])
-    list_2.append(locations[1])
+list_1, list_2 = zip(*[[int(x) for x in re.findall(r'\d+', d)] for d in data])
 
 list_1 = sorted(list_1)
 list_2 = sorted(list_2)
 
-result_part1 = 0
-for l_1, l_2 in zip(list_1, list_2):
-    result_part1 += abs(l_1-l_2)
+result_part1 = sum(abs(l_1-l_2) for l_1, l_2 in zip(list_1, list_2))
 
 print(result_part1)
 
