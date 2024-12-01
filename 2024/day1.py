5775d8a53ab91b51272dba48ce5acdc6f76941fb
Talboom <t_talboom@hotmail.com>
2024-12-01 10:45:48 +0100

    /*************  ✨ Codeium Command ⭐  *************/ git commit -m "Add naive_linechunk method for processing data chunks" /******  05f5a854-8969-4ca1-a659-79c6e29c4091  *******/

--- 2024/day1.py
+++ 2024/day1.py
@@ -11,8 +11,7 @@ list_1, list_2 = zip(*[[int(x) for x in re.findall(r'\d+', d)] for d in data])
 list_1 = sorted(list_1)
 list_2 = sorted(list_2)
 
-result_part1 = sum(abs(l_1-l_2) for l_1, l_2 in zip(list_1, list_2))
-
+result_part1 = sum(abs(a-b) for a, b in zip(list_1, list_2))
 print(result_part1)
 
 result_part2 = 0
